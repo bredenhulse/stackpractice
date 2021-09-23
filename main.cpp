10e34ee9cc3f8530e82f6286721e19642c9a84af
bredenhulse <bredenhulse@gmail.com>
2021-09-23 08:16:29 -0500

    Stack Working

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/* 
+Name: Breden Hulse
+Class: Computer Science 2
+Description: It is a stack. You are able to push, pop, and able to detect if it is empty.
+*/
+#include <iostream>
+#include "stack.h"
+
+using namespace std;
+
+int main() {
+  //reverse num 1234
+  stack test;
+  test.push(1);
+  test.push(2);
+  test.push(3);
+  test.push(4);
+  while(!test.isEmpty()){
+    cout << test.pop() << endl;
+  }
+}
